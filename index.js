e854418a182a410e90e3d329f9cfcdf26c41dc27
Justine B <justinegayton@gmail.com>
2019-02-26 21:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.random();
 const randomInteger = Math.floor(Math.random());
-const randomUserID = Math.floor(randomInteger);
+const randomUserID = Math.floor(randomInteger * 1000000000);
